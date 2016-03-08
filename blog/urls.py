3ccc288d64622b91c6b78c65a351199974026e34
Kim Sung Hwan <3dartmax@naver.com>
2016-03-08 20:54:37 +0900

    테스트 템플릿 단축url추가

--- blog/urls.py
+++ blog/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	#url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),	
-	url(r'^post/test1_1/$', views.post_test1_1),
-	url(r'^post/test1_2/$', views.post_test1_2),
-	url(r'^post/test1_3/$', views.post_test1_3),
-	url(r'^post/test1_4/$', views.post_test1_4),
+	url(r'^post/test1_1/$', views.post_test1_1, name='post_test1_1'),
+	url(r'^post/test1_2/$', views.post_test1_2, name='post_test1_2'),
+	url(r'^post/test1_3/$', views.post_test1_3, name='post_test1_3'),
+	url(r'^post/test1_4/$', views.post_test1_4, name='post_test1_4'),
 ]
